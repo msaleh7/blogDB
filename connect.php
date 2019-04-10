ad2b202c39f7a73c2aad040f5e32a104f7fbfc16
msaleh7 <49427869+msaleh7@users.noreply.github.com>
2019-04-09 22:16:36 -0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
-mysqli_real_connect($conn, 'mysqldb7.mysql.database.azure.com', 'msaleh7@mysqldb7', 'Msm@9810959', 'blogdb', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'mysqldb7.mysql.database.azure.com', 'phpappuser', 'pass123', 'blogdb', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
